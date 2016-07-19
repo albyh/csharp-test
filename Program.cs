9a57da2f7ad98c602a6cb0c86d95196d1822a153
albyh <albyheredia@gmail.com>
2016-07-19 10:33:14 -0700

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 
+
 namespace Program1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            // Prompt user to enter a name.
+            Console.WriteLine("Enter your name, please:");
+            // Now read the name entered
+            string name = Console.ReadLine();
+            // Greet the user with the entered name
+            Console.WriteLine("Hello, " + name);
+            // Wait for the user to acknowledge
+            Console.WriteLine("Press Enter to quit...");
+            Console.Read();
         }
     }
 }
